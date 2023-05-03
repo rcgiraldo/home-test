7d55ca1266ca02015419f0c865a3d9b9f44ae669
Ruben Giraldo <ru.giraldo@gmail.com>
2023-05-02 22:01:38 -0500

    Added unit tests for the service

--- src/commits/commits.service.spec.ts
+++ src/commits/commits.service.spec.ts
@@ -1,6 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CommitsService } from './commits.service';
 
+
 describe('CommitsService', () => {
   let service: CommitsService;
 
@@ -15,4 +16,20 @@ describe('CommitsService', () => {
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  describe('findAll', () => {
+	it('should return an array of commits', async () => {
+		const commits = await service.findAll();
+		expect(Array.isArray(commits)).toBe(true);
+	})
+  });
+
+  describe('findOne', () => {
+	it('should get a single commit', async() => {
+		const commit = await service.findOne("6d93f34b14fb732965f227b88a955750b4379ff3");
+		expect(commit.author.date).toEqual("2023-04-29T11:56:27Z");
+
+	})
+  })
+
 });
